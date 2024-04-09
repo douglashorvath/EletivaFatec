b708f6ab79413b5194d36f1a78d66cfde1393e40
DOUGLAS HORVATH <douglhorvath@gmail.com>
2024-04-08 21:54:19 -0300

    PHP-Lista2 v1

---
+++ PHP_Lista2/exercicio03resposta.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03</title>
+    <style>
+        header{
+            color: blue;
+        }
+        main{
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <header>
+    <h1>Exercício 03</h1>
+    <h3>Digite 10 valores numéricos e armazene em um mapa ordenado. Em seguida, solicite ao usuário um número para multiplicar todos os elementos do vetor. 
+        O programa deverá exibir o resultado da multiplicação do número dado pelo usuário em todos os elementos armazenados. </h3>
+    </header>
+    <main>
+    <?php 
+    
+    
+    ?>
+
+    </main>
+</body>
+</html>
---
+++ PHP_Lista2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista 2 de Exercícios PHP</title>
+</head>
+<body>
+    <h1>Lista 1 de Exercícios PHP</h1>
+    <p><a href="exercicio01.php"><h2>Exercicio 01</h2></a></p>
+    <p><a href="exercicio02.php"><h2>Exercicio 02</h2></a></p>
+    <p><a href="exercicio03.php"><h2>Exercicio 03</h2></a></p>
+    <p><a href="exercicio04.php"><h2>Exercicio 04</h2></a></p>
+    <p><a href="exercicio05.php"><h2>Exercicio 05</h2></a></p>
+    <p><a href="exercicio06.php"><h2>Exercicio 06</h2></a></p>
+    <p><a href="exercicio07.php"><h2>Exercicio 07</h2></a></p>
+    
+</body>
+</html>
